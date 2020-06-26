3c57417bf05b37d15d39716ae9d824f9936bfa44
smreferee <66741871+smreferee@users.noreply.github.com>
2020-06-26 14:21:33 -0500

    Add files via upload

---
+++ Lab4_Richard_Nolan.py
@@ -0,0 +1,54 @@
+################################################
+#This is Lab 4 for COSC 1336 by Richard Nolan  # 
+#The program will display a menu, ask a menu   #
+#choice, and play the Change Game if selected. #
+################################################
+
+#Introduction to the program
+print('Hi! Welcome to the Lame Game!')
+
+
+#Game Menu
+print("GAME MENU")
+print('1. Make Change')
+print('2. High Card')
+print('3. Quit')
+choice = int(input('Choice: '))
+
+
+###MenuChoiceResponses
+
+#Change Game
+if (choice==1):
+    print ('Make Change!')
+    #Queries for money owed and paid
+    owed = float(input('Customer owes: '))
+    paid = float(input('Customer paid: '))
+    
+    #Insufficient payment message
+    if paid < owed:
+        print("You didn't pay enough!")
+    else:
+        
+    #Prints Change
+    change = float(format(paid-owed,'.2f'))
+    print('$',change, sep='')
+    
+    #Prints Change Denomination
+    print (int(change//1), "dollars")
+    change = change%1
+    print (int(change//.25), "quarters")
+    change = change%.25
+    print (int(change//.10), "dimes")
+    change = change%.10
+    print (int(change//.05), "nickles")
+    change = change%.05
+    print (round(change/.01), "pennies")
+    
+#High Card Notice   
+elif (choice==2):
+    print('High Card Coming Soon!')
+    
+#Invalid Option
+elif (choice>3 or choice<1):
+    print('Invalid Option!');

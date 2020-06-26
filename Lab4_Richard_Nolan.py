8fc4e51617923e4060a1a6d734afba24175682cd
smreferee <66741871+smreferee@users.noreply.github.com>
2020-06-26 15:08:59 -0500

    Add files via upload

--- Lab4_Richard_Nolan.py
+++ Lab4_Richard_Nolan.py
@@ -57,7 +57,45 @@ else:
     
        #High Card Notice   
         elif (choice==2):
-        print('High Card')
+        print('High Card Game')
+
+        #Player Names
+        Player_1 = input("Enter Player 1's name: ")
+        Player_2 = input("Enter Player 2's name: ")
+
+        #Draw random cards
+        card_1 = random.randint(1,13)
+            if card_1 == 1:
+            face_1 = 'Ace'
+            elif card_1 == 2:
+            face_1 =('Two')
+            elif card_1 == 3:
+            face_1 = ('Three')
+            elif card_1 == 4:
+            face_1 =('Four')        
+            elif card_1 == 5:
+            face_1 =('Five')        
+            elif card_1 == 6:
+            face_1 =('Six')       
+            elif card_1 == 7:
+            face_1 =('Seven')        
+            elif card_1 == 8:
+            face_1 =('Eight')        
+            elif card_1 == 9:
+            face_1 =('Nine')
+            elif card_1 == 10:
+            face_1 =('Ten')        
+            elif card_1 == 11:
+            face_1 =('Jack')        
+            elif card_1 == 12:
+            face_1 =('Queen')     
+            elif card_1 == 13:
+            face_1 =('King')
+        card_2 = random.randint(1,13)
+
+        #Print Players' names and cards
+        print(Player_1, 'drew a', face_1)
+        print(Player_2, 'drew a', face_2)
 
     #Sentinel Check
     print("GAME MENU")
